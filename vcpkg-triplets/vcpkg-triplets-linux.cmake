c0e8cb1c71f9d6e4e010cd1739b888f030770492
pbdot <125748236+pbdot@users.noreply.github.com>
2024-11-24 09:21:40 -0500

    Switching to vcpkg for deps, cmake tweaks

---
+++ vcpkg-triplets/vcpkg-triplets-linux.cmake
@@ -0,0 +1,4 @@
+
+set(VCPKG_TARGET_TRIPLET x64-linux)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
---
+++ vcpkg-triplets/vcpkg-triplets-windows.cmake
@@ -0,0 +1,4 @@
+
+set(VCPKG_TARGET_TRIPLET x64-windows-static)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
